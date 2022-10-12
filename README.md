317fee8f46c087963a39329053fcfe2c62a0ad10
Bilal Nasar <bnasar15@gmail.com>
2022-10-12 00:38:41 -0400

    added styling & left sidebar links

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Twitter (quasar-project)
 
+Edited by Bilal Nasar
+
 A Quasar Project
 
 ## Install the dependencies
+
 ```bash
 yarn
 # or
@@ -10,15 +13,17 @@ npm install
 ```
 
 ### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
 ```bash
 quasar dev
 ```
 
-
 ### Build the app for production
+
 ```bash
 quasar build
 ```
 
 ### Customize the configuration
+
 See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js).
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,23 +1,49 @@
 <template>
   <q-layout view="lHr lpR fFf">
 
-    <q-header bordered class="bg-primary text-white">
+    <q-header bordered class="bg-white text-black">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+        <q-toolbar-title class="text-weight-bolder">
+          Twitter
         </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+      <q-icon
+      class="q-pa-md"
+       name="fa-solid fa-dove"
+       size="lg"
+       color="primary"
+        />
+        <q-list>
+      <q-item
+      to="/"
+      clickable
+      v-ripple
+      >
+        <q-item-section class="text-h6
+        text-weight-bold">Home</q-item-section>
+        <q-item-section avatar>
+          <q-icon  name="home" size="md" />
+        </q-item-section>
+      </q-item>
+      <q-item
+      to="/about"
+      clickable
+      v-ripple
+      >
+        <q-item-section class="text-h6
+        text-weight-bold">About</q-item-section>
+        <q-item-section avatar>
+          <q-icon  name="help" size="md" />
+        </q-item-section>
+      </q-item>
+
+
+    </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
